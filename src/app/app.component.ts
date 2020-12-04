5f21f2f57f0273c2c9987a1adebb06c4c22ac396
AndreaAlfarano <andrea.alfarano95@gmail.com>
2020-12-04 14:51:43 +0100

    Inserisco titolo3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   template: `
   <h1>Titolo</h1>
   <h1>titolo2</h1>
+  <h1>Titolo 3 </h1>
     <div class="container">
       <form
         class="card card-body mt-2"
